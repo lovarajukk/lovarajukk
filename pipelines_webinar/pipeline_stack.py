709eb53127ab9e3ba543009850a77dc63d5b6df6
lovarajukk <lovaraju.kk@gmail.com>
2021-12-05 13:06:42 -0500

    updated repo

--- pipelines_webinar/pipeline_stack.py
+++ pipelines_webinar/pipeline_stack.py
@@ -2,8 +2,9 @@ from aws_cdk import core
 from aws_cdk import aws_codepipeline as codepipeline
 from aws_cdk import aws_codepipeline_actions as cpactions
 from aws_cdk import pipelines
-class PipelineStack(core.stack):
-    def__init__(self,scope: core.Construct,id:str,**kwargs)
+from .webservice_stage import WebServiceStage
+class PipelineStack(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs):
         super().__init__(scope,id,**kwargs)
 
         source_artifact = code.Artifact()
@@ -15,7 +16,7 @@ class PipelineStack(core.stack):
           action_name='GitHub',
           output=source_artifact,
          oauth_token=core.SecretValue.secrets_manager('github-token'),
-        owner='lovarajukk'
+        owner='lovarajukk',
         repo='lovarajukk',
         trigger=cpactions.GitHubTrigger.POLL),
         synth_action=pipelines.SimpleSynthAction(
