a9fba86f82c031b44a3de5aa9659fc9fe00510e4
lovarajukk <lovaraju.kk@gmail.com>
2021-12-08 08:45:28 -0500

    webservice created

--- pipelines_webinar/webservice_stage.py
+++ pipelines_webinar/webservice_stage.py
@@ -2,7 +2,7 @@ from aws_cdk import core
 from .pipelines_webinar_stack import PipelineWebinarStack
 class WebServiceStage(core.Stage):
     def __init__(self, scope: core.Construct, id: str, **kwargs):
-    super.()def__init__(scope ,id,**kwargs)
+      super().__init__(scope,id,**kwargs)
 
     service = PipelineWebinarStack(self,'webservice')
     self.url_output = self.url_output
